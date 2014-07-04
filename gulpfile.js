89dc76b0b6b381b7832d2cf588cca30f2fa77c2a
Phips Peter <pspeter333@gmail.com>
2014-07-04 09:22:28 -0700

    Adding gulp for testing
    Fairly standard lint and spec rules.

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
+var path = require('path');
+
+var root = path.join(__dirname, '*.js');
+var lib = path.join(__dirname, 'lib', '**', '*.js');
+var tests = path.join(__dirname, 'test', '**', '*.js');
+
+gulp.task('lint', function() {
+  return gulp.src([root, lib, tests])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('spec', function() {
+  return gulp.src(tests)
+    .pipe(mocha({
+      reporter: 'spec'
+    }));
+});
+
+gulp.task('test', ['lint', 'spec']);
