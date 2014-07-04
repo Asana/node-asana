aa83db3556a042f39637a5a63738a21092ccc3f8
Phips Peter <pspeter333@gmail.com>
2014-07-04 16:15:28 -0700

    Adding basic auth static factory

--- test/client_spec.js
+++ test/client_spec.js
@@ -4,11 +4,23 @@ var assert = require('assert');
 var Client = require('../lib/client');
 
 describe('Client', function() {
+  describe('basicAuth', function() {
+    it('should return a basic auth client', function() {
+      var apiKey = 'apiKey';
+      var client = Client.basicAuth(apiKey);
+      assert.equal(client.authKey, 'auth');
+      assert.deepEqual(client.authValue, {
+        user: apiKey,
+        pass: ''
+      });
+    });
+  });
+  
   describe('#new', function() {
     it('should have the auth key and auth value', function() {
       var authKey = 'auth';
       var authValue = {
-        user: 'apikey',
+        user: 'apiKey',
         pass: ''
       };
       var client = new Client(authKey, authValue);
