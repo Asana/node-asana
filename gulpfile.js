4bf85ce799902b2fccc913f16f67fb10f0b956f5
Phips Peter <pspeter333@gmail.com>
2014-07-06 16:13:44 -0700

    Added github pages to documentation
    This is now viewable at http://pspeter3.com/node-asana

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+var clean = require('gulp-clean');
+var ghPages = require('gulp-gh-pages');
 var gulp = require('gulp');
 var jsdoc = require('gulp-jsdoc');
 var jshint = require('gulp-jshint');
@@ -5,13 +7,25 @@ var mocha = require('gulp-mocha');
 var path = require('path');
 var runSequence = require('run-sequence');
 
+var docs = path.join(__dirname, 'docs');
+var documentation = path.join(__dirname, 'docs', '**', '*.*');
 var root = path.join(__dirname, '*.js');
 var lib = path.join(__dirname, 'lib', '**', '*.js');
 var tests = path.join(__dirname, 'test', '**', '*.js');
 
-gulp.task('docs', function() {
-  gulp.src(lib)
+gulp.task('clean', function() {
+  return gulp.src(docs)
+    .pipe(clean());
+});
+
+gulp.task('docs', ['clean'], function() {
+  return gulp.src(lib)
     .pipe(jsdoc('./docs'));
+});
+
+gulp.task('pages', ['docs'], function() {
+  return gulp.src(documentation)
+    .pipe(ghPages());
 });
 
 gulp.task('lint', function() {
