a20e1ea106ea9141fd435972205cfdd96b685230
Phips Peter <pspeter333@gmail.com>
2014-07-15 11:10:50 -0700

    Cleaning up the build and test scripts
    We no longer need to publish to coveralls. It would be nice to make sure
    that code coverage is above a certain threshold though.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-var coveralls = require('gulp-coveralls');
 var gulp = require('gulp');
 var istanbul = require('gulp-istanbul');
 var jshint = require('gulp-jshint');
@@ -7,14 +6,10 @@ var path = require('path');
 var runSequence = require('run-sequence');
 var wicked = require('wicked');
 
-var readme = path.join(__dirname, 'README.md');
 var index = path.join(__dirname, 'index.js');
-var docs = path.join(__dirname, 'docs');
-var documentation = path.join(__dirname, 'docs', '**', '*.*');
 var root = path.join(__dirname, '*.js');
 var lib = path.join(__dirname, 'lib', '**', '*.js');
 var tests = path.join(__dirname, 'test', '**', '*.js');
-var lcov = path.join(__dirname, 'coverage', 'lcov.info');
 
 gulp.task('docs', function(callback) {
   wicked(null, null, callback);
@@ -41,11 +36,6 @@ gulp.task('spec', function(callback) {
     });
 });
 
-gulp.task('coverage', function() {
-  gulp.src(lcov)
-    .pipe(coveralls());
-});
-
 gulp.task('test', function(callback) {
-  runSequence('lint', 'spec', 'coverage', callback);
+  runSequence('lint', 'spec', callback);
 });
