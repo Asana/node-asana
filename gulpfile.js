e86b0b1be8226d95f658616039c7e91f64e1aac0
greg <greg@asana.com>
2015-01-20 14:04:47 -0800

    Add gulp task to bump version for both npm and bower

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,16 @@ var gulp = require('gulp');
 var istanbul = require('gulp-istanbul');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
+var shell = require('gulp-shell');
 var uglify = require('gulp-uglify');
 var vinylBuffer = require('vinyl-buffer');
 var vinylSourceStream = require('vinyl-source-stream');
+
+//xcxc
+var git = require('gulp-git');
+var bump = require('gulp-bump');
+var filter = require('gulp-filter');
+var tagVersion = require('gulp-tag-version');
 
 /**
  * Paths
@@ -43,6 +50,38 @@ function browserTask(minify) {
 }
 gulp.task('browser', browserTask(false));
 gulp.task('browser-min', browserTask(true));
+
+/**
+ * Bumping version number and tagging the repository with it.
+ * Please read http://semver.org/
+ *
+ * You can use the commands
+ *
+ *     gulp bump-patch     # makes v0.1.0 → v0.1.1
+ *     gulp bump-feature   # makes v0.1.1 → v0.2.0
+ *     gulp bump-release   # makes v0.2.1 → v1.0.0
+ *
+ * To bump the version numbers accordingly after you did a patch,
+ * introduced a feature or made a backwards-incompatible release.
+ */
+function bumpVersion(importance) {
+  return gulp.src(['./package.json', './bower.json'])
+      .pipe(bump({type: importance}))
+      .pipe(gulp.dest('./'))
+      .pipe(git.commit('bumps package version'))
+      .pipe(filter('package.json'))
+      .pipe(tagVersion());
+}
+gulp.task('bump-patch', function() { return bumpVersion('patch'); });
+gulp.task('bump-feature', function() { return bumpVersion('minor'); });
+gulp.task('bump-release', function() { return bumpVersion('major'); });
+
+/**
+ * Publishes to bower
+ */
+gulp.task('publish-bower', function() {
+  gulp.src(['./package.json', './bower.json'])
+});
 
 /**
  * Lints all of the JavaScript files and fails if the tasks do not pass
