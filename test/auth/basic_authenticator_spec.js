9cc2131cdea9e1ee39f4e11e2203ac0b3575209c
saagar <saagar@asana.com>
2015-01-21 16:54:14 -0800

    tests for basic and oauth authenticators

---
+++ test/auth/basic_authenticator_spec.js
@@ -0,0 +1,35 @@
+/* jshint mocha:true */
+var assert = require('assert');
+var rewire = require('rewire');
+var BasicAuthenticator = rewire('../../lib/auth/basic_authenticator');
+
+describe('BasicAuthenticator', function() {
+
+  describe('#authenticateRequest', function() {
+
+    it('should store api key', function() {
+      var basicAuthenticator = new BasicAuthenticator('apikey');
+      assert.equal(basicAuthenticator.apiKey, 'apikey');
+    });
+
+    it('should add auth to request', function() {
+      var basicAuthenticator = new BasicAuthenticator('apikey');
+      var request = basicAuthenticator.authenticateRequest({});
+      assert.equal(request.auth.username, 'apikey');
+      assert.equal(request.auth.password, '');
+    });
+
+  });
+
+  describe('#ensureCredentials', function() {
+
+    it('should return a resolved Promise', function(){
+      var basicAuthenticator = new BasicAuthenticator('apikey');
+
+      var p = basicAuthenticator.ensureCredentials();
+      assert.equal(true, p.isResolved());
+    });
+
+  });
+
+});
