01114be321635ec04cbd301d8ef7cfed8a921b04
greg <greg@asana.com>
2015-05-22 00:11:57 -0700

    Add `Client.all` which fetches all results in a collection by going through pages

--- test/util/paging_spec.js
+++ test/util/paging_spec.js
@@ -4,6 +4,7 @@ var sinon = require('sinon');
 var paging = require('../../lib/util/paging.js');
 
 describe('Paging', function() {
+
   describe('#nextPage', function() {
 
     it('should return null if no results', function() {
@@ -65,6 +66,89 @@ describe('Paging', function() {
         paging.nextPage(response, dispatcher);
       }, /response/i);
       assert(!dispatcher.dispatch.called);
+    });
+
+  });
+
+  describe('#all', function() {
+
+    function dispatcherThatReturns(response) {
+      var dispatcher = { dispatch: sinon.stub() };
+      var fakePromise = { then: sinon.stub() };
+      dispatcher.dispatch.returns(fakePromise);
+      fakePromise.then.onFirstCall().callsArgWith(0, response);
+      return dispatcher;
+    }
+
+    it('should return empty if no results', function(done) {
+      var response = {
+        'data': [],
+        'next_page': null
+      };
+      var dispatcher = {
+        dispatch: sinon.stub()
+      };
+      paging.all(response, dispatcher).then(function(results) {
+        assert.deepEqual(results, []);
+        done();
+      });
+    });
+
+    it('should return dispatcher result for next url', function(done) {
+      var response1 = {
+        'data': ['a', 'b'],
+        'next_page': {
+          'uri': 'fake_url'
+        }
+      };
+      var response2 = {
+        'data': ['c', 'd'],
+        'next_page': null
+      };
+      var dispatcher = dispatcherThatReturns(response2);
+      paging.all(response1, dispatcher).then(function(results) {
+        assert.deepEqual(['a','b','c','d'], results);
+        done();
+      });
+    });
+
+    it('should pass dispatch options', function(done) {
+      var response = {
+        'data': [],
+        'next_page': {
+          'uri': 'fake_url'
+        }
+      };
+      var dispatcher = dispatcherThatReturns({
+        'data': [],
+        'next_page': null
+      });
+      var fakeOptions = {
+        fake: 'value'
+      };
+      paging.all(response, dispatcher, fakeOptions).then(function() {
+        assert.deepEqual(dispatcher.dispatch.firstCall.args[1], fakeOptions);
+        done();
+      });
+    });
+
+    it('should fail on unrecognized response', function(done) {
+      var response = {
+        //'data': {}
+      };
+      var dispatcher = {
+        dispatch: sinon.stub()
+      };
+      paging.all(response, dispatcher)
+          .then(function() {
+            assert.fail('Failed to throw error on bad response');
+          })
+          .catch(function() {
+            assert(!dispatcher.dispatch.called);
+          })
+          .finally(function() {
+            done();
+          });
     });
 
   });
