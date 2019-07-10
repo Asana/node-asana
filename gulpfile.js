f66286bedf8f176839b28af77896b8ffd65dab45
Ross Grambo <rossgrambo@asana.com>
2019-07-10 14:16:50 -0700

    Revert "Fixed security vulnerabilities"
    This reverts commit ec9b320e94ae7f9b4166cd45b66b3787bb208980.

--- gulpfile.js
+++ gulpfile.js
@@ -20,10 +20,16 @@ var root = '*.js';
 var test = 'test/**/*.js';
 
 /**
+ * High Level Tasks
+ */
+gulp.task('test', ['bundle', 'spec']);
+gulp.task('bundle', ['browser', 'browser-min']);
+
+/**
  * Bundles the code, full version to `asana.js` and minified to `asana-min.js`
  */
 function browserTask(minify) {
-  return function(done) {
+  return function() {
     var task = browserify(
         {
           entries: [index],
@@ -36,26 +42,11 @@ function browserTask(minify) {
           .pipe(vinylBuffer())
           .pipe(uglify());
     }
-    task.pipe(gulp.dest('dist'));
-
-    done();
+    return task.pipe(gulp.dest('dist'));
   };
 }
-
-/**
- * Ensure that the git working directory is clean.
- */
-function ensureGitClean(done) {
-  git.status(function(err, out) {
-    if (err) { throw err; }
-    if (!/working directory clean/.exec(out)) {
-      throw new Error('Git working directory not clean, will not bump version');
-    }
-  });
-
-  done();
-}
-gulp.task('ensure-git-clean', ensureGitClean);
+gulp.task('browser', browserTask(false));
+gulp.task('browser-min', browserTask(true));
 
 /**
  * Bumping version number and tagging the repository with it.
@@ -78,68 +69,65 @@ function bumpVersion(importance) {
       .pipe(filter('package.json'))
       .pipe(tagVersion());
 }
-function bumpPatch() {
+gulp.task('bump-patch', ['ensure-git-clean'], function() {
   return bumpVersion('patch');
-}
-gulp.task('bump-patch', gulp.series('ensure-git-clean', bumpPatch));
-function bumpFeature() {
+});
+gulp.task('bump-feature', ['ensure-git-clean'], function() {
   return bumpVersion('minor');
-}
-gulp.task('bump-feature', gulp.series('ensure-git-clean', bumpFeature));
-function bumpVersion() {
+});
+gulp.task('bump-release', ['ensure-git-clean'], function() {
   return bumpVersion('major');
-}
-gulp.task('bump-release', gulp.series('ensure-git-clean', bumpVersion));
+});
+
+/**
+ * Ensure that the git working directory is clean.
+ */
+gulp.task('ensure-git-clean', function() {
+  git.status(function(err, out) {
+    if (err) { throw err; }
+    if (!/working directory clean/.exec(out)) {
+      throw new Error('Git working directory not clean, will not bump version');
+    }
+  });
+});
 
 /**
  * Lints all of the JavaScript files and fails if the tasks do not pass
  */
-function lint() {
+gulp.task('lint', function() {
   return gulp.src([root, lib, test])
-      .pipe(jshint())
-      .pipe(jshint.reporter('jshint-stylish'))
-      .pipe(jshint.reporter('fail'));
-}
-gulp.task('lint', lint);
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
+});
 
 /**
  * Tests the code with mocha and ensures 100% code coverage
  */
-function spec(callback) {
+gulp.task('spec', ['lint'], function(callback) {
   gulp.src(lib)
-      .pipe(istanbul({
-        includeUntested: true
-      }))
-      .pipe(istanbul.hookRequire())
-      .on('finish', function() {
-        gulp.src(test)
-            .pipe(mocha({
-              reporter: process.env.TRAVIS ? 'spec' : 'nyan'
-            }))
-            .pipe(istanbul.writeReports({
-              reporters: ['text', 'text-summary']
-            }))
-            .on('end', function() {
-              var errOrNull = null;
-              var coverage = istanbul.summarizeCoverage();
-              var incomplete = Object.keys(coverage).filter(function(key) {
-                return coverage[key].pct < 49; // TODO: Get this to 100%
-              });
-              if (incomplete.length > 0) {
-                console.log('Incomplete coverage for ' + incomplete.join(', '));
-              }
-              callback(errOrNull);
-            });
-      });
-}
-
-gulp.task('spec', gulp.series('lint', spec));
-
-gulp.task('browser', browserTask(false));
-gulp.task('browser-min', browserTask(true));
-
-/**
- * High Level Tasks
- */
-gulp.task('bundle', gulp.series('browser', 'browser-min'));
-gulp.task('test', gulp.series('bundle', 'spec'));
+    .pipe(istanbul({
+      includeUntested: true
+    }))
+    .pipe(istanbul.hookRequire())
+    .on('finish', function() {
+      gulp.src(test)
+        .pipe(mocha({
+          reporter: process.env.TRAVIS ? 'spec' : 'nyan'
+        }))
+        .pipe(istanbul.writeReports({
+          reporters: ['text', 'text-summary']
+        }))
+        .on('end', function() {
+          var errOrNull = null;
+          var coverage = istanbul.summarizeCoverage();
+          var incomplete = Object.keys(coverage).filter(function(key) {
+            return coverage[key].pct < 49; // TODO: Get this to 100%
+          });
+          if (incomplete.length > 0) {
+            console.log('Incomplete coverage for ' + incomplete.join(', '));
+          }
+          callback(errOrNull);
+        });
+    });
+});
